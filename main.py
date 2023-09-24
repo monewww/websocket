d80532e95890300197fef015fd1f383c194c9d3a
DESKTOP-B8IEJH1\c'y'k <2155822524@qq.com>
2023-09-24 15:57:05 +0800

    balala

---
+++ main.py
@@ -0,0 +1,15 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+ip_adress='124.71.100.30'
+port=6666
+server_socket.bind((ip_adress,port))
+server_socket.setblocking(False)
+server_socket.listen()
+while True:
+    try:
+        text=server_socket.accept()
+        print(text)
+    except BlockingIOError:
+        pass
