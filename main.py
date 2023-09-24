42f066085a7da8f08ebf32928a9bfae32d5a2831
DESKTOP-B8IEJH1\c'y'k <2155822524@qq.com>
2023-09-24 16:04:56 +0800

    hhh

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -8,8 +9,10 @@ server_socket.bind((ip_adress,port))
 server_socket.setblocking(False)
 server_socket.listen()
 while True:
+    time.sleep(0.5)
     try:
         text=server_socket.accept()
         print(text)
     except BlockingIOError:
+        print("waiting")
         pass
