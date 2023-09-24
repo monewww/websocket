40ef340716f5eb47cd13d97593c4592140362429
DESKTOP-B8IEJH1\c'y'k <2155822524@qq.com>
2023-09-24 16:13:38 +0800

    hh

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-ip_adress='localhost'
+ip_adress='124.71.100.30'
 port=6666
 server_socket.bind((ip_adress,port))
 server_socket.setblocking(False)
