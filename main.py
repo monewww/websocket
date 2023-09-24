73805cc8726558f936f93ee2b0af681245d1e55c
DESKTOP-B8IEJH1\c'y'k <2155822524@qq.com>
2023-09-24 16:01:58 +0800

    test

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-ip_adress='124.71.100.30'
+ip_adress='192.168.0.205'
 port=6666
 server_socket.bind((ip_adress,port))
 server_socket.setblocking(False)
